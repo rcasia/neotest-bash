7d8919ff8d4ae50d3ef100688021f75c68f3cc0a
Ricardo Casía <ricardocasia.dev@gmail.com>
2023-11-08 00:52:35 +0100

    test: discovers test that have the 'function' keyword (#4)

--- tests/core/position_discoverer_spec.lua
+++ tests/core/position_discoverer_spec.lua
@@ -14,12 +14,15 @@ describe("PositionDiscoverer", function()
 		--then
 		-- local test_file_node = tree:to_list()[1]
 		local test_function_node = tree:to_list()[2][1]
+		local test_function_node_2 = tree:to_list()[3][1]
 		local actual_test_name = test_function_node.name
+		local actual_test_name_2 = test_function_node_2.name
 
 		assert.are.equal("test_sum_1_plus_1", actual_test_name)
+		assert.are.equal("test_sum_2_plus_2", actual_test_name_2)
 
 		-- there is only one test function
-		assert.are.equal(1, #tree:children())
+		assert.are.equal(2, #tree:children())
 	end)
 
 	async.it("ignores functions that are not tests", function()
