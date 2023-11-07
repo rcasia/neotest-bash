17b67134c2498cf5b1a6862d679c102ef6fa024f
Ricardo Casía <ricardocasia.dev@gmail.com>
2023-11-08 00:28:31 +0100

    feat: ignores functions that are not tests

--- tests/core/position_discoverer_spec.lua
+++ tests/core/position_discoverer_spec.lua
@@ -4,9 +4,8 @@ local async = require("nio").tests
 local current_dir = vim.fn.expand("%:p:h")
 
 describe("PositionDiscoverer", function()
-	async.it("discovers normal test", function()
+	async.it("discovers test with prefix 'test_' ", function()
 		--given
-		local expected_test_name = "test_sum_1_plus_1"
 		local filename = current_dir .. "/tests/fixtures/example_test.sh"
 
 		--when
@@ -17,6 +16,22 @@ describe("PositionDiscoverer", function()
 		local test_function_node = tree:to_list()[2][1]
 		local actual_test_name = test_function_node.name
 
-		assert.are.equal(expected_test_name, actual_test_name)
+		assert.are.equal("test_sum_1_plus_1", actual_test_name)
+
+		-- there is only one test function
+		assert.are.equal(1, #tree:children())
+	end)
+
+	async.it("ignores functions that are not tests", function()
+		--given
+		local filename = current_dir .. "/tests/fixtures/no_test_functions_here_test.sh"
+
+		--when
+		local tree = plugin.discover_positions(filename)
+
+		--then
+		local children = tree:children()
+
+		assert.are.equal(0, #children)
 	end)
 end)
---
+++ tests/fixtures/no_test_functions_here_test.sh
@@ -0,0 +1,6 @@
+
+
+this_is_not_a_test() {
+  echo "This is not a test"
+}
+
