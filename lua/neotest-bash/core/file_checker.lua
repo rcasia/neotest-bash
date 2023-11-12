04e070eb66fe331020aa0908a40e3805a3210896
Ricardo Casía <ricardocasia.dev@gmail.com>
2023-11-12 17:22:34 +0100

    feat: includes testfiles that end with 'test.sh' (#8)

--- lua/neotest-bash/core/file_checker.lua
+++ lua/neotest-bash/core/file_checker.lua
@@ -4,7 +4,8 @@ FileChecker = {}
 ---@param file_path string
 ---@return boolean
 function FileChecker.isTestFile(file_path)
-	return false
+	-- test files end with test.sh
+	return file_path:match("test.sh$")
 end
 
 return FileChecker
