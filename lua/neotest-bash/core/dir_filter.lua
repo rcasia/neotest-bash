bb395a46761a8f05ade6d5e2c5237a774578d0f5
Ricardo Casía <ricardocasia.dev@gmail.com>
2023-11-12 16:03:23 +0100

    feat: filters by one directory when DEFAULT_PATH is file

--- lua/neotest-bash/core/dir_filter.lua
+++ lua/neotest-bash/core/dir_filter.lua
@@ -2,6 +2,13 @@ local read_dot_env = require("neotest-bash.core.util.read_dot_env")
 
 DirFilter = {}
 
+local function is_file(path)
+	-- check if it's a file with regex
+	-- example: .../some_test.sh
+	-- should end with _test.sh
+	return string.match(path, "_test.sh$")
+end
+
 ---Filter directories when searching for test files
 ---@async
 ---@param name string Name of directory
@@ -12,12 +19,19 @@ function DirFilter.filter_dir(name, rel_path, root)
 	-- read .env file
 	local dot_env_properties = read_dot_env(root .. ".env")
 
-	local test_dir = dot_env_properties["DEFAULT_PATH"]
-	if test_dir == nil then
+	local test_path = dot_env_properties["DEFAULT_PATH"]
+	if test_path == nil then
 		return true -- allow all directories
 	end
 
-	if string.match(rel_path, test_dir) then
+	if is_file(test_path) then
+		-- get rid of the file name
+		-- example: tests/some_test.sh -> tests/
+		local test_dir = string.match(test_path, "^(.*/)")
+		return test_dir == rel_path -- allow only the directory of the file
+	end
+
+	if string.find(rel_path, test_path) ~= nil then
 		return true -- allow specified directories
 	end
 
--- tests/core/dir_filter_spec.lua
+++ tests/core/dir_filter_spec.lua
@@ -5,10 +5,36 @@ local async = require("nio").tests
 
 local root = vim.fn.fnamemodify("tests/fixtures/demo-project/", ":p")
 
+local function create_temporary_dir()
+	-- create a temporary directory
+	local tmp_dir = "/tmp/neotest-bash/tests/"
+	os.execute("mkdir -p " .. tmp_dir)
+	return tmp_dir
+end
+
+local function remove_dir()
+	-- delete the temporary directory
+	os.execute("rm -rf /tmp/neotest-bash/tests/")
+end
+
+--- @param dir string
+--- @param env table
+local function create_env_file(dir, env)
+	local env_file = dir .. ".env"
+	os.execute("touch " .. env_file)
+	for k, v in pairs(env) do
+		os.execute("echo " .. k .. "=" .. v .. " >> " .. env_file)
+	end
+end
+
 describe("dir_filter", function()
+	after_each(function()
+		remove_dir()
+	end)
+
 	local allowed_dirs = {
 		"tests/",
-		"tests/core",
+		"tests/core/",
 	}
 	for _, dir in ipairs(allowed_dirs) do
 		async.it("should allow with .env file => (DEFAULT_PATH=tests): " .. dir, function()
@@ -35,20 +61,36 @@ describe("dir_filter", function()
 
 	-- local all_dirs = table.insert(not_allowed_dirs, allowed_dirs)
 	-- merge tables
-	local all_dirs = { vim.tbl_extend("force", allowed_dirs, not_allowed_dirs) }
+	local all_dirs = vim.tbl_extend("force", allowed_dirs, not_allowed_dirs)
 	for _, dir in ipairs(all_dirs) do
-		async.it("should allow all directories when there is no .env file", function()
+		async.it("should allow all directories when there is no .env file: " .. dir, function()
 			-- create a temporary directory
-			local tmp_dir = vim.fn.tempname()
+			local tmp_root = create_temporary_dir()
 
-			vim.fn.mkdir(tmp_dir, "p")
-
-			local is_allowed = plugin.filter_dir("name1", dir, tmp_dir)
-
-			--delete the temporary directory
-			vim.fn.delete(tmp_dir, "rf")
+			local is_allowed = plugin.filter_dir("name1", dir, tmp_root)
 
 			assert.is_true(is_allowed)
 		end)
 	end
+
+	local dirs = {
+		["tests/core/"] = true,
+		["tests/core/deeper/"] = false,
+	}
+	for dir, expected in pairs(dirs) do
+		async.it("should allow only one directory when DEFAULT_PATH is a test file: " .. dir, function()
+			local name = "name1" -- doesn't matter
+			-- create a temporary directory
+			local tmp_root = create_temporary_dir()
+			create_env_file(tmp_root, { ["DEFAULT_PATH"] = "tests/core/logic_test.sh" })
+
+			local is_allowed = plugin.filter_dir(name, dir, tmp_root)
+
+			assert.is_equal(expected, is_allowed)
+		end)
+	end
+
+  it("playing around", function()
+	  print(string.find("core/tests/", "core") ~= nil)
+  end)
 end)
