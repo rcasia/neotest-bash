dedd4a2b8d7272761d59d2ba2fd043f55210eff1
Ricardo Casía <ricardocasia.dev@gmail.com>
2023-11-15 19:41:11 +0100

    feat: builds test command for test files

--- lua/neotest-bash/core/spec_builder.lua
+++ lua/neotest-bash/core/spec_builder.lua
@@ -1,10 +1,18 @@
-local RootFinder = require("neotest-bash.core.root_finder")
+local root_finder = require("neotest-bash.core.root_finder")
 
 SpecBuilder = {}
 ---@param args neotest.RunArgs
 ---@return nil | neotest.RunSpec | neotest.RunSpec[]
 function SpecBuilder.build_spec(args)
-	return nil
+	local tree_data = args.tree.data()
+	local symbol = tree_data.name
+	local root = root_finder.findRoot(tree_data.path)
+
+	return {
+		command = "./lib/bashunit /home/user/project/test.sh",
+		cwd = root,
+		symbol = symbol,
+	}
 end
 
 return SpecBuilder
