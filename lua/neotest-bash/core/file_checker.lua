216b3df8b528eceeab80e4718c0304a7b511db0f
Ricardo Casía <31012661+rcasia@users.noreply.github.com>
2025-12-19 10:23:16 +0100

    chore: random change

--- lua/neotest-bash/core/file_checker.lua
+++ lua/neotest-bash/core/file_checker.lua
@@ -4,6 +4,7 @@ FileChecker = {}
 ---@param file_path string
 ---@return boolean
 function FileChecker.isTestFile(file_path)
+	local a = 0
 	-- test files end with test.sh
 	return file_path:match("test.sh$")
 end
